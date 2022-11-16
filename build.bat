63657318806e4881aaa918d45e1a4058ad771196
AliFARESS <116644939+AliFARESS@users.noreply.github.com>
2022-11-16 11:10:13 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 mkdir debug
 cd debug
 
-cmake -G "Eclipse CDT4 - MinGW Makefiles" -D"CMAKE_MAKE_PROGRAM:PATH=C:\tools\i686-12.2.0-release-posix-dwarf-rt_v10-rev1\mingw32\bin\mingw32-make.exe" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE -DCMAKE_ECLIPSE_MAKE_ARGUMENTS=-j8 ../src
+cmake -G "Eclipse CDT4 - MinGW Makefiles" -D"CMAKE_MAKE_PROGRAM:PATH=cmake/Toolchain/mingw32-make.exe" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE -DCMAKE_ECLIPSE_MAKE_ARGUMENTS=-j8 ../src
 
 cd ..
 
