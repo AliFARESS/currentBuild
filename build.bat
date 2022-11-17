9a1df3fa43e926ae3ca1e5ef4c327b3967cbbb9e
AliFARESS <116644939+AliFARESS@users.noreply.github.com>
2022-11-17 14:21:38 +0100

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,9 @@
+mkdir build
+
+cd build
+
+cmake -G "MinGW Makefiles" -D"CMAKE_MAKE_PROGRAM:PATH=cmake/Toolchain/mingw32-make.exe" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE -DCMAKE_ECLIPSE_MAKE_ARGUMENTS=-j8 ./src
+
+cmake --build ./build
+
+
