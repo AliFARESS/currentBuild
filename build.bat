9530cff6f6d36c42100815d9c795d9042c75c461
AliFARESS <116644939+AliFARESS@users.noreply.github.com>
2022-11-17 13:36:22 +0100

    Delete build.bat

--- build.bat
+++
@@ -1,6 +0,0 @@
-cmake -G "MinGW Makefiles" -D"CMAKE_MAKE_PROGRAM:PATH=C:/tools/i686-12.2.0-release-posix-dwarf-rt_v10-rev1/mingw32/bin/mingw32-make.exe" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE -DCMAKE_ECLIPSE_MAKE_ARGUMENTS=-j8 ../src
-
-cmake --build ./debug
-
-
-
